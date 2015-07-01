829634441a0c0db37f263566c45750f2aab9cabb
Justas <jmedeisis@gmail.com>
2015-07-01 12:25:49 +0100

    Load and visualise muscle activations (forces) using line width and color parameters. Integrate experimental orbital camera.

--- Assets/MuscleDataLoader.cs
+++ Assets/MuscleDataLoader.cs
@@ -7,20 +7,27 @@ using System.Collections.Generic;
 
 public class MuscleDataLoader {
 
-    public Vector3[][] LoadMusclePaths()
+    private const string outputPath =
+                "C:\\Users\\Justas\\SkyDrive\\FreeBodyVis\\For Justas\\FreeBody App\\example"
+                + "\\1037_C14\\walking6\\Outputs\\";
+    private const int numMuscleElements = 163; // distinct muscles
+
+    public Vector3[][] LoadMusclePaths(out int[] vertexToMuscle)
     {
         List<List<Vector3>> musclePathsPerFrame = new List<List<Vector3>>();
+        List<int> vertexToMuscleList = new List<int>();
 
-        // 162 distinct muscles
-        for (int i = 0; i < 162; i++)
+        for (int i = 0; i < numMuscleElements; i++)
         {
-            string path =
-                "C:\\Users\\Justas\\SkyDrive\\FreeBodyVis\\For Justas\\FreeBody App\\example"
-                + "\\1037_C14\\walking6\\Outputs\\Muscle_geometry\\"
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+            string path = outputPath + "Muscle_geometry\\"
                 + String.Format("1037_walking6_c14_new_muscle_path{0}.csv", i);
-
-            //Debug.Log("Reading file " + path);
             Vector3[][] musclePath = ReadMuscleDataFile(path);
+#else
+            string path = String.Format("c14_demo_{0}", i);
+            Vector3[][] musclePath = ReadMuscleDataFileResources(path);
+#endif
+
             for (int j = 0; j < musclePath.Length; j++)
             {
                 if (musclePathsPerFrame.Count <= j)
@@ -36,6 +43,11 @@ public class MuscleDataLoader {
                     segmentedLines.Add(musclePath[j][k]);
                 }
 
+                for (int k = 0; k < segmentedLines.Count; k++)
+                {
+                    vertexToMuscleList.Add(i);
+                }
+
                 musclePathsPerFrame[j].AddRange(segmentedLines);
             }
         }
@@ -47,6 +59,7 @@ public class MuscleDataLoader {
             musclePathsPerFrameFinal[i] = musclePathsPerFrame[i].ToArray();
         }
 
+        vertexToMuscle = vertexToMuscleList.ToArray();
         return musclePathsPerFrameFinal;
     }
 
@@ -54,21 +67,65 @@ public class MuscleDataLoader {
     {
         List<Vector3[]> frames = new List<Vector3[]>();
 
-        string line;
         using (StreamReader reader = new StreamReader(fileName, Encoding.Default))
         {
+            ReadVertices(reader, frames);
+            reader.Close();
+        }
+
+        return frames.ToArray();
+    }
+
+    private Vector3[][] ReadMuscleDataFileResources(string filename)
+    {
+        List<Vector3[]> frames = new List<Vector3[]>();
+
+        TextAsset file = Resources.Load(filename) as TextAsset;
+
+        using (StringReader reader = new StringReader(file.text))
+        {
+            ReadVertices(reader, frames);
+            reader.Close();
+        }
+
+        return frames.ToArray();
+    }
+
+    private void ReadVertices(TextReader reader, List<Vector3[]> frames)
+    {
+        string line = reader.ReadLine();
+        while (null != line)
+        {
+            float[] values = Array.ConvertAll(line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries),
+                new Converter<string, float>(float.Parse));
+
+            Vector3[] vertices = new Vector3[values.Length / 3];
+            for (int i = 0; i < values.Length; i += 3)
+            {
+                vertices[i / 3] = new Vector3(values[i + 0], values[i + 1], values[i + 2]);
+            }
+            frames.Add(vertices);
+
             line = reader.ReadLine();
+        }
+    }
+
+    public float[][] LoadMuscleActivations()
+    {
+        List<float[]> activations = new List<float[]>();
+
+        string fileName = outputPath + "Optimisation\\" + "1037_walking6_c14_new_force_gcs.csv";
+        using (StreamReader reader = new StreamReader(fileName, Encoding.Default))
+        {
+            string line = reader.ReadLine();
             while (null != line)
             {
                 float[] values = Array.ConvertAll(line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries),
                     new Converter<string, float>(float.Parse));
 
-                Vector3[] vertices = new Vector3[values.Length / 3];
-                for (int i = 0; i < values.Length; i += 3)
-                {
-                    vertices[i / 3] = new Vector3(values[i + 0], values[i + 1], values[i + 2]);
-                }
-                frames.Add(vertices);
+                float[] rawActivations = new float[numMuscleElements];
+                Array.Copy(values, rawActivations, numMuscleElements);
+                activations.Add(rawActivations);
 
                 line = reader.ReadLine();
             }
@@ -76,6 +133,40 @@ public class MuscleDataLoader {
             reader.Close();
         }
 
-        return frames.ToArray();
+        // TODO figure out what's up dog.. why are 2 frames missing??..
+        activations.Add(new float[numMuscleElements]);
+        activations.Add(new float[numMuscleElements]);
+
+        fileName = outputPath + "Optimisation\\" + "1037_walking6_c14_new_force_ub.csv";
+        int frame = -1;
+        float maxActivationValue = float.MinValue;
+        float minActivationValue = float.MaxValue;
+        using (StreamReader reader = new StreamReader(fileName, Encoding.Default))
+        {
+            string line = reader.ReadLine();
+            while (null != line)
+            {
+                ++frame;
+                double[] values = Array.ConvertAll(line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries),
+                    new Converter<string, double>(double.Parse));
+
+                for (int i = 0; i < numMuscleElements; i++)
+                {
+                    if (values[i] != 0) activations[frame][i] = (float)(activations[frame][i] / values[i]);
+                    maxActivationValue = Math.Max(maxActivationValue, activations[frame][i]);
+                    minActivationValue = Math.Min(minActivationValue, activations[frame][i]);
+
+                    activations[frame][i] = Math.Min(activations[frame][i], 1f); // cap at 1
+                }
+
+                line = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        Debug.Log("Activation values loaded min " + minActivationValue + " max " + maxActivationValue + ".");
+        if(maxActivationValue > 1) Debug.Log("Activation values capped to 1.");
+
+        return activations.ToArray();
     }
 }
--- Assets/MuscleMesh.cs
+++ Assets/MuscleMesh.cs
@@ -11,12 +11,15 @@ public class MuscleMesh : MonoBehaviour {
     public Shader shader;
 
     private MuscleDataLoader loader;
-    private Vector3[][] line;
+    private Vector3[][] lines;
+    private float[][] lineWeights;
+    private Color[][] lineColors;
 
     private int frame;
     private float accumulator;
     private const float secondsPerAnimationFrame = 0.016f;
-    private const float lineWidth = 0.01f;
+    private const float minLineWidth = 0.002f;
+    private const float maxLineWidth = 0.01f;
 
     void Awake()
     {
@@ -33,15 +36,13 @@ public class MuscleMesh : MonoBehaviour {
 
     void Start()
     {
-        line = loader.LoadMusclePaths();
+        int[] vertexToMuscle;
+        lines = loader.LoadMusclePaths(out vertexToMuscle);
+        float[][] muscleForce = loader.LoadMuscleActivations();
+        lineWeights = CalculateLineWeights(vertexToMuscle, muscleForce);
+        lineColors = CalculateLineColors(vertexToMuscle, muscleForce);
 
-        /*
-        lines = new Vector3[] {
-            new Vector3(0, 0, -5), new Vector3(3, 5, -3),
-            new Vector3(0, 0, -1), new Vector3(-3, 4, -3),
-            new Vector3(-4, 2, -2), new Vector3(1, -1, -1)};
-        */
-        AddLines(line[0], lineWidth);
+        AddLines(lines[0], lineWeights[0], lineColors[0]); // frame 0
         frame = 0;
     }
 	
@@ -50,14 +51,14 @@ public class MuscleMesh : MonoBehaviour {
         accumulator += Time.deltaTime;
         while (accumulator >= secondsPerAnimationFrame)
         {
-            frame = (frame + 1) % line.Length;
+            frame = (frame + 1) % lines.Length;
             accumulator -= secondsPerAnimationFrame;
         }
-        UpdateLines(line[frame], lineWidth);
+        UpdateLines(lines[frame], lineWeights[frame], lineColors[frame]);
 	}
 
     // lines == line endpoints, not continuous, size = 2 x #lines
-    void AddLines(Vector3[] lines, float lineWidth)
+    void AddLines(Vector3[] lines, float[] lineWeights, Color[] lineColors)
     {
         Debug.Log("Adding " + lines.Length / 2 + " lines.");
 
@@ -68,12 +69,15 @@ public class MuscleMesh : MonoBehaviour {
         for (int i = 0; i < lines.Length; i += 2)
         {
             int vi = (i / 2) * 4; // vertex start index for line quad
-            UpdateQuad(vertices, vi, lines[i], lines[i + 1], lineWidth);
+            UpdateQuad(vertices, vi, lines[i], lines[i + 1], lineWeights[i]);
+            for (int j = 0; j < 4; j++)
+            {
+                colors[vi + j] = lineColors[i];
+            }
             System.Array.Copy(new int[] {
                 vi + 0, vi + 1, vi + 2,
                 vi + 1, vi + 3, vi + 2 },
                 0, triangles, (i / 2) * 6, 6);
-            System.Array.Copy(new Color[] { Color.red, Color.red, Color.red, Color.red }, 0, colors, vi, 4);
         }
 
         mesh.vertices = vertices;
@@ -82,20 +86,24 @@ public class MuscleMesh : MonoBehaviour {
         mesh.RecalculateBounds();
     }
 
-    void UpdateLines(Vector3[] lines, float lineWidth)
+    void UpdateLines(Vector3[] lines, float[] lineWeights, Color[] lineColors)
     {
         Vector3[] vertices = mesh.vertices;
-        //int[] triangles = mesh.triangles;
-        //Color[] colors = mesh.colors;
+        Color[] colors = mesh.colors;
 
         for (int i = 0; i < lines.Length; i += 2)
         {
             int vi = (i / 2) * 4; // vertex start index for line quad
-            UpdateQuad(vertices, vi, lines[i], lines[i + 1], lineWidth);
+            UpdateQuad(vertices, vi, lines[i], lines[i + 1], lineWeights[i]);
+            for (int j = 0; j < 4; j++)
+            {
+                colors[vi + j] = lineColors[i];
+            }
         }
 
         mesh.vertices = vertices;
         mesh.RecalculateBounds();
+        mesh.colors = colors;
     }
 
     void UpdateQuad(Vector3[] vertices, int start, Vector3 p1, Vector3 p2, float width)
@@ -112,5 +120,41 @@ public class MuscleMesh : MonoBehaviour {
         vertices[start + 3] = p2 - l * halfWidth;
 
         //vertex = transform.InverseTransformPoint(vertex); // unnecessary
+    }
+
+    // per frame per vertex
+    static float[][] CalculateLineWeights(int[] vertexToMuscle, float[][] muscleForce)
+    {
+        float[][] lineWeights = new float[muscleForce.Length][];
+
+        for (int i = 0; i < lineWeights.Length; i++)
+        {
+            lineWeights[i] = new float[vertexToMuscle.Length];
+            for (int j = 0; j < vertexToMuscle.Length; j++)
+            {
+                lineWeights[i][j] = minLineWidth + muscleForce[i][vertexToMuscle[j]] * (maxLineWidth - minLineWidth);
+                //lineWeights[i][j] = Mathf.Max(minLineWidth, Mathf.Min(lineWeights[i][j], maxLineWidth));
+            }
+        }
+
+        return lineWeights;
+    }
+
+    // per frame per vertex
+    static Color[][] CalculateLineColors(int[] vertexToMuscle, float[][] muscleForce)
+    {
+        Color[][] lineColors = new Color[muscleForce.Length][];
+
+        for (int i = 0; i < lineColors.Length; i++)
+        {
+            lineColors[i] = new Color[vertexToMuscle.Length];
+            for (int j = 0; j < vertexToMuscle.Length; j++)
+            {
+                lineColors[i][j] = //muscleForce[i][vertexToMuscle[j]] > 0.005f ? Color.red : Color.gray;
+                                    Color.Lerp(Color.black, Color.red, muscleForce[i][vertexToMuscle[j]]);
+            }
+        }
+
+        return lineColors;
     }
 }
