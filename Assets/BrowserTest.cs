f73f49b6e9e3109334c4c9be7966fd3ee822a8d8
Justas <jmedeisis@gmail.com>
2015-07-02 14:22:37 +0100

    Experiment with free third-party Unity file browsers.

---
+++ Assets/BrowserTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrowserTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Browser browser = GetComponent<Browser>();
+        browser.OpenFile("C:\\Users\\Justas\\SkyDrive\\FreeBodyVis\\For Justas\\FreeBody App\\example"
+                + "\\1037_C14\\walking6\\Outputs\\");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
