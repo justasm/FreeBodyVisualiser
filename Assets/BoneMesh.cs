abf90ef9130e1efe83d307dd62516c6c48973280
Justas <jmedeisis@gmail.com>
2015-07-03 12:09:58 +0100

    Load femur at runtime and render procedurally generated mesh instead of manually imported one.

---
+++ Assets/BoneMesh.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
+public class BoneMesh : MonoBehaviour {
+
+    private MeshFilter meshFilter;
+    public Shader shader;
+
+    void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = new Material(shader);
+    }
+
+    void Start()
+    {
+        Mesh mesh = StlFileReader.LoadStlFile(DataPathUtils.GetFemurModelFile());
+        mesh.name = "Bone";
+        meshFilter.mesh = mesh;
+
+        transform.localScale = 0.001f * Vector3.one;
+    }
+
+    void Update()
+    {
+        
+    }
+}
