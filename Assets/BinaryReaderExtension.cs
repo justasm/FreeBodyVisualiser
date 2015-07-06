36e92cf1aab080839741df2b1249954d53cdc0af
Justas <jmedeisis@gmail.com>
2015-07-06 12:20:28 +0100

    Add support for loading ASCII STL files.

---
+++ Assets/BinaryReaderExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text;
+
+/**
+ * Original at http://stackoverflow.com/a/27571627/1200090
+ */
+public static class BinaryReaderExtension {
+
+    public static string ReadLine(this BinaryReader reader)
+    {
+        if (reader.IsEndOfStream())
+            return null;
+
+        StringBuilder result = new StringBuilder();
+        char character;
+        while (!reader.IsEndOfStream() && (character = reader.ReadChar()) != '\n')
+        {
+            if (character != '\r' && character != '\n')
+            {
+                result.Append(character);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    public static bool IsEndOfStream(this BinaryReader reader)
+    {
+        return reader.BaseStream.Position == reader.BaseStream.Length;
+    }
+}
