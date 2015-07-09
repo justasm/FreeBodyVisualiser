6be2221ae010a3f25474069455fe727e48eae637
Justas <jmedeisis@gmail.com>
2015-07-09 13:49:05 +0100

    Add editor utility to clear player preferences.

---
+++ Assets/Editor/EditorUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class EditorUtils {
+
+    [MenuItem("Edit/Reset Playerprefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+}
